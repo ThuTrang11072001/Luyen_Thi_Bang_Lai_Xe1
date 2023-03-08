d49b12b02b68e35beca7341506eb0d90bdce9a41
Lê Thi Thu Trang <trang2k1vp@gmail.com>
2023-03-09 04:10:34 +0700

    giao diẹn

--- app/src/main/java/com/example/luynthibnglixeb1/MainActivity.java
+++ app/src/main/java/com/example/luynthibnglixeb1/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.luynthibnglixeb1;
 
+import static android.app.ProgressDialog.show;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent();
+                i.setClass(MainActivity.this,
+                        Activity2.class);
+
+                startActivity(i);
+
+            }
+        });
     }
 }
